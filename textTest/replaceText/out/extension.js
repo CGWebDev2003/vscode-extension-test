72511cbd89cfd9f9ba5b3de3f94eb659be063cd6
3M <Marius006@gmx.de>
2023-03-02 05:34:53 +0100

    ....

--- textTest/replaceText/out/extension.js
+++ textTest/replaceText/out/extension.js
@@ -4,6 +4,20 @@ exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
     const disposable = vscode.commands.registerCommand('extension.reverseWord', function () {
+        // const editor = vscode.window.activeTextEditor;
+        // if(editor) {
+        //     let document = editor.document;
+        //     let selection = editor.selection;
+        //     let curPos = editor.selection.active;
+        //     let curLine = curPos.line+1;
+        //     let curChar = curPos.character+1;
+        //     editor.edit(editBuilder => {
+        //         editBuilder.insert(curPos, "function newFunction(param) {\n    console.log(param);\n }");
+        //     });    
+        // }
+        // context.subscriptions.push(disposable);
+    });
+    vscode.commands.registerCommand('extension.addFunction', () => {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
             let document = editor.document;
@@ -12,7 +26,7 @@ function activate(context) {
             let curLine = curPos.line + 1;
             let curChar = curPos.character + 1;
             editor.edit(editBuilder => {
-                editBuilder.insert(curPos, "function newFunction(param) {\n    console.log(param);\n }");
+                editBuilder.insert(curPos, "function newFunction(param) {\n    console.log(param);\n}");
             });
         }
         context.subscriptions.push(disposable);
--- textTest/replaceText/src/extension.ts
+++ textTest/replaceText/src/extension.ts
@@ -5,6 +5,26 @@ import { Position } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('extension.reverseWord', function() {
+        // const editor = vscode.window.activeTextEditor;
+
+        // if(editor) {
+        //     let document = editor.document;
+        //     let selection = editor.selection;
+        //     let curPos = editor.selection.active;
+        //     let curLine = curPos.line+1;
+        //     let curChar = curPos.character+1;
+
+
+        //     editor.edit(editBuilder => {
+        //         editBuilder.insert(curPos, "function newFunction(param) {\n    console.log(param);\n }");
+        //     });    
+        // }
+
+        // context.subscriptions.push(disposable);
+    });
+
+
+    vscode.commands.registerCommand('extension.addFunction', () => {
         const editor = vscode.window.activeTextEditor;
 
         if(editor) {
@@ -16,11 +36,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 
             editor.edit(editBuilder => {
-                editBuilder.insert(curPos, "function newFunction(param) {\n    console.log(param);\n }");
+                editBuilder.insert(curPos, "function newFunction(param) {\n    console.log(param);\n}");
             });    
         }
 
         context.subscriptions.push(disposable);
-    }
+    });
 
-)}
+
+}
